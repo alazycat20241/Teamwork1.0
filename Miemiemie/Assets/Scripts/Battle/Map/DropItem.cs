158fc870f7082caa9a2de57b4a2e0b172f58a028
alazycat20241 <3038887636@qq.com>
2026-06-14 20:04:48 +0800

    掉落物和音量调节

---
+++ Miemiemie/Assets/Scripts/Battle/Map/DropItem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public enum CollectibleType { Gold, SoulStone, halfLove,fullLove }
+
+    [SerializeField] private CollectibleType type;
+    [SerializeField] private int amount = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("碰到了");
+        if (!other.CompareTag("Player")) return;
+        Health health = other.GetComponent<Health>();
+        if (PlayerInventory.Instance == null) return;
+
+        switch (type)
+        {
+            case CollectibleType.Gold:
+                PlayerInventory.Instance.AddGold(amount);
+                break;
+            case CollectibleType.SoulStone:
+                PlayerInventory.Instance.soulStones += amount;
+                PlayerInventory.Instance.UpdateStone();
+                break;
+            case CollectibleType.halfLove:
+                if(health!=null)health.currentHealth += 5;
+                break;
+            case CollectibleType.fullLove:
+                if(health != null)health.currentHealth += 10;
+                break;
+        }
+
+        Destroy(gameObject);
+    }
+}
